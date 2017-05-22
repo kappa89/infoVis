290cffc9dc5178b4d6b97df86b8b1059476ac192
kappa <kappa89@gmail.com>
2017-05-22 19:34:19 +0200

    Now uses all available space

--- script/script.js
+++ script/script.js
@@ -1,11 +1,23 @@
-var width = 960,
-    height = 500;
+var svg = d3.select("svg");
+function getRealSVGDimensions(){
+	var winH = window.innerHeight;
+	var winW = window.innerWidth;
+	var svgPercH = svg.attr("height").slice(0, -1);
+	var svgPercW = svg.attr("width").slice(0, -1);
 
+	return {"H":(winH*(svgPercH/100.0))-50, "W": (winW*(svgPercW/100.0))-50};
+}
+
+var svgDimensions = getRealSVGDimensions();
+
+var width = svgDimensions.W,
+    height = svgDimensions.H;
+var numberOfFlowers = 30;
 var flowerRadius = 30;
 var globalCenter = [width/2.0,
                     height/2.0];
 
-var nodes = d3.range(30).map(function() { return {radius : "a"}; }),
+var nodes = d3.range(numberOfFlowers+1).map(function() { return {}; }),
     root = nodes[0];
 
 root.radius = 50;
@@ -13,29 +25,17 @@ root.fixed = true;
 
 var force = d3.layout.force()
     .gravity(0.0)
-    .charge(function(d,i){return i ? 0 : -500;})
+    .charge(function(d,i){return i ? -0.01 : -500;})
     .nodes(nodes)
     .size([width, height]);
 
 force.start();
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-    
-    svg.append("rect")
-    .attr("width", width)
-    .attr("height", height)
-    .style("stroke", "black")
-    .style("fill", "none")
-    .style("stroke-width", 3);
-
-
 svg.selectAll("image")
     .data(nodes.slice(1)) //no root
     .enter().append("svg:image")
     .attr("xlink:href", "img/clover.svg")
-    .attr("height", "10%");
+    .attr("height", flowerRadius);
 
 function setCenter(index)
 {
@@ -49,14 +49,14 @@ function setCenter(index)
 force.on("tick", function(e) {
  svg.selectAll("image")
       .attr("x", function(d, i) {
-          if((d.x > (width - flowerRadius)) || d.x < flowerRadius)
+          if((d.x > (width - flowerRadius)) || d.x < 0)
           {
               return setCenter(i);
           }
           return d.x;
         })
       .attr("y", function(d, i) {
-         if(d.y > height-flowerRadius || d.y < flowerRadius)
+         if(d.y > (height-flowerRadius) || d.y < 0)
          {
               return setCenter(i);
           }
